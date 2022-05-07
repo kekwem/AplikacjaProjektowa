723c7c5eb19b918f6b6d8c7c29d28f3024e94fe5
kekwem <93526125+kekwem@users.noreply.github.com>
2022-05-07 23:55:35 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace aplikacjaProjecktowa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.szukajbutton = new System.Windows.Forms.Button();
+            this.analizujbutton = new System.Windows.Forms.Button();
+            this.katalogibutton = new System.Windows.Forms.Button();
+            this.plikibutton = new System.Windows.Forms.Button();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.sortNameButton = new System.Windows.Forms.Button();
+            this.sortDataButton = new System.Windows.Forms.Button();
+            this.sortRozmiarButton = new System.Windows.Forms.Button();
+            this.zliczanieButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(12, 72);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(1076, 484);
+            this.listBox.TabIndex = 0;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(12, 35);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(1076, 22);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // szukajbutton
+            // 
+            this.szukajbutton.Location = new System.Drawing.Point(1125, 34);
+            this.szukajbutton.Name = "szukajbutton";
+            this.szukajbutton.Size = new System.Drawing.Size(92, 23);
+            this.szukajbutton.TabIndex = 2;
+            this.szukajbutton.Text = "Szukaj";
+            this.szukajbutton.UseVisualStyleBackColor = true;
+            this.szukajbutton.Click += new System.EventHandler(this.szukajbutton_Click);
+            // 
+            // analizujbutton
+            // 
+            this.analizujbutton.Location = new System.Drawing.Point(1125, 72);
+            this.analizujbutton.Name = "analizujbutton";
+            this.analizujbutton.Size = new System.Drawing.Size(92, 23);
+            this.analizujbutton.TabIndex = 3;
+            this.analizujbutton.Text = "Ananlizuj";
+            this.analizujbutton.UseVisualStyleBackColor = true;
+            this.analizujbutton.Click += new System.EventHandler(this.analizujbutton_Click);
+            // 
+            // katalogibutton
+            // 
+            this.katalogibutton.Location = new System.Drawing.Point(1125, 111);
+            this.katalogibutton.Name = "katalogibutton";
+            this.katalogibutton.Size = new System.Drawing.Size(92, 23);
+            this.katalogibutton.TabIndex = 4;
+            this.katalogibutton.Text = "Katalogi";
+            this.katalogibutton.UseVisualStyleBackColor = true;
+            this.katalogibutton.Click += new System.EventHandler(this.katalogibutton_Click);
+            // 
+            // plikibutton
+            // 
+            this.plikibutton.Location = new System.Drawing.Point(1125, 151);
+            this.plikibutton.Name = "plikibutton";
+            this.plikibutton.Size = new System.Drawing.Size(92, 23);
+            this.plikibutton.TabIndex = 5;
+            this.plikibutton.Text = "Pliki";
+            this.plikibutton.UseVisualStyleBackColor = true;
+            this.plikibutton.Click += new System.EventHandler(this.plikibutton_Click);
+            // 
+            // sortNameButton
+            // 
+            this.sortNameButton.Location = new System.Drawing.Point(1125, 191);
+            this.sortNameButton.Name = "sortNameButton";
+            this.sortNameButton.Size = new System.Drawing.Size(92, 43);
+            this.sortNameButton.TabIndex = 6;
+            this.sortNameButton.Text = "Sort imiona";
+            this.sortNameButton.UseVisualStyleBackColor = true;
+            this.sortNameButton.Click += new System.EventHandler(this.sortNameButton_Click);
+            // 
+            // sortDataButton
+            // 
+            this.sortDataButton.Location = new System.Drawing.Point(1125, 249);
+            this.sortDataButton.Name = "sortDataButton";
+            this.sortDataButton.Size = new System.Drawing.Size(92, 43);
+            this.sortDataButton.TabIndex = 7;
+            this.sortDataButton.Text = "Sort daty";
+            this.sortDataButton.UseVisualStyleBackColor = true;
+            this.sortDataButton.Click += new System.EventHandler(this.sortDataButton_Click);
+            // 
+            // sortRozmiarButton
+            // 
+            this.sortRozmiarButton.Location = new System.Drawing.Point(1125, 375);
+            this.sortRozmiarButton.Name = "sortRozmiarButton";
+            this.sortRozmiarButton.Size = new System.Drawing.Size(92, 43);
+            this.sortRozmiarButton.TabIndex = 8;
+            this.sortRozmiarButton.Text = "Sort rozmiar";
+            this.sortRozmiarButton.UseVisualStyleBackColor = true;
+            this.sortRozmiarButton.Click += new System.EventHandler(this.sortRozmiarButton_Click);
+            // 
+            // zliczanieButton
+            // 
+            this.zliczanieButton.Location = new System.Drawing.Point(1125, 448);
+            this.zliczanieButton.Name = "zliczanieButton";
+            this.zliczanieButton.Size = new System.Drawing.Size(92, 61);
+            this.zliczanieButton.TabIndex = 9;
+            this.zliczanieButton.Text = "Zlicznie plikow po typu";
+            this.zliczanieButton.UseVisualStyleBackColor = true;
+            this.zliczanieButton.Click += new System.EventHandler(this.zliczanieButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1122, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tylko dla plikow";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(247, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Jaki katalog albo plik chcesz znalesz?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 577);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zliczanieButton);
+            this.Controls.Add(this.sortRozmiarButton);
+            this.Controls.Add(this.sortDataButton);
+            this.Controls.Add(this.sortNameButton);
+            this.Controls.Add(this.plikibutton);
+            this.Controls.Add(this.katalogibutton);
+            this.Controls.Add(this.analizujbutton);
+            this.Controls.Add(this.szukajbutton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Pliki i katalogi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button szukajbutton;
+        private System.Windows.Forms.Button analizujbutton;
+        private System.Windows.Forms.Button katalogibutton;
+        private System.Windows.Forms.Button plikibutton;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.Button sortNameButton;
+        private System.Windows.Forms.Button sortDataButton;
+        private System.Windows.Forms.Button sortRozmiarButton;
+        private System.Windows.Forms.Button zliczanieButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aplikacjaProjecktowa
+{
+    public partial class Form1 : Form
+    {
+        List<FileInfo> lista_plikow = new List<FileInfo>();
+        List<DirectoryInfo> lista_katalogow = new List<DirectoryInfo>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            fbd.Description = "Wybierz katalog, w którym będzie dzialal algorytm";
+            fbd.ShowNewFolderButton = false;
+        }
+        //Aplikacja do analiz i wyszukiwania plików i folderów, generująca także różne statystyki
+        //np. wypisywanie plików, folderów posortowanych wg rozmiarów, daty utworzenia, zliczanie plików wg ich typów (rozszerzeń),
+        public void refresh()
+        {
+            listBox.Items.Clear();
+            lista_plikow.RemoveRange(0, lista_plikow.Count);
+            lista_katalogow.RemoveRange(0, lista_katalogow.Count);
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            searchTextBox.Text = "";
+        }
+
+        private void szukajbutton_Click(object sender, EventArgs e)
+        {
+            string napis = searchTextBox.Text;
+            refresh();
+            searchTextBox.Text = napis;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string katalog = fbd.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(katalog);
+                if (searchTextBox.Text.Equals(""))
+                {
+                    searchTextBox.Text = "Tutaj trzeba wpisac nazwe pliku albo katalogu ktorego szukassz!";
+                }
+                else
+                {
+                    szukaj(directoryInfo, searchTextBox.Text);
+                    wyswietlanie();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano katalogu do wyszukiwania");
+            }
+        }
+
+        private void szukaj(DirectoryInfo di, string pattern)
+        {
+            FileInfo[] pliki = di.GetFiles();
+            foreach (FileInfo fi in pliki)
+            {
+                if (fi.Name.Contains(pattern))
+                {
+                    lista_plikow.Add(fi);
+                }
+            }
+            DirectoryInfo[] katalogi = di.GetDirectories();
+            foreach (DirectoryInfo dikolejne in katalogi)
+            {
+                if (dikolejne.Name.Contains(pattern))
+                {
+                    lista_katalogow.Add(dikolejne);
+                }
+                szukaj(dikolejne, pattern);
+            }
+        }
+
+        private void analizujbutton_Click(object sender, EventArgs e)
+        {
+            refresh();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string katalog = fbd.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(katalog);
+                listBox.Items.Add($"{directoryInfo.Name} data utworzenia: {directoryInfo.CreationTime}");
+                analizujKatalog(directoryInfo);
+                searchTextBox.Text = "";
+                if (lista_katalogow.Count != 0)
+                {
+                    searchTextBox.Text += $"Liczba katalogow: {lista_katalogow.Count} ";
+                }
+                if (lista_plikow.Count != 0)
+                {
+                    searchTextBox.Text += $"Liczba plikow: {lista_plikow.Count}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano katalogu do wyszukiwania");
+            }
+        }
+
+        private void analizujKatalog(DirectoryInfo di)
+        {
+            FileInfo[] pliki = di.GetFiles();
+            listBox.Items.Add($"Pliki w katalogu ---> {di.FullName}: ");
+            foreach (FileInfo fi in pliki)
+            {
+                lista_plikow.Add(fi);
+                listBox.Items.Add($"{fi.Name} czas utworzenia: {fi.CreationTime} rozmiar pliku: {fi.Length}");
+            }
+            DirectoryInfo[] katalogi = di.GetDirectories();
+            foreach (DirectoryInfo dikolejne in katalogi)
+            {
+                lista_katalogow.Add(dikolejne);
+                analizujKatalog(dikolejne);
+            }
+        }
+
+        private void katalogibutton_Click(object sender, EventArgs e)
+        {
+            refresh();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string katalog = fbd.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(katalog);
+                szukajKatalogi(directoryInfo);
+                wyswietlanie();
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano katalogu do wyszukiwania");
+            }
+        }
+
+        private void szukajKatalogi(DirectoryInfo di)
+        {
+            DirectoryInfo[] katalogi = di.GetDirectories();
+            foreach (DirectoryInfo dikolejne in katalogi)
+            {
+                lista_katalogow.Add(dikolejne);
+                szukajKatalogi(dikolejne);
+            }
+        }
+
+        private void plikibutton_Click(object sender, EventArgs e)
+        {
+            refresh();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string katalog = fbd.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(katalog);
+                szukajPliki(directoryInfo);
+                wyswietlanie();
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano katalogu do wyszukiwania");
+            }
+        }
+
+        private void szukajPliki(DirectoryInfo di)
+        {
+
+            FileInfo[] pliki = di.GetFiles();
+            foreach (FileInfo fi in pliki)
+            {
+                lista_plikow.Add(fi);
+            }
+            DirectoryInfo[] katalogi = di.GetDirectories();
+            foreach (DirectoryInfo dikolejne in katalogi)
+            {
+                szukajPliki(dikolejne);
+            }
+        }
+
+        private void sortNameButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            lista_katalogow.Sort(
+                delegate (DirectoryInfo dr1, DirectoryInfo dr2) { return dr1.Name.CompareTo(dr2.Name); }
+            );
+            lista_plikow.Sort(
+                delegate (FileInfo fi1, FileInfo fi2) { return fi1.Name.CompareTo(fi2.Name); }
+            );
+            wyswietlanie();
+        }
+
+        private void sortDataButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            lista_katalogow.Sort(
+                delegate (DirectoryInfo dr1, DirectoryInfo dr2) { return dr1.CreationTime.CompareTo(dr2.CreationTime); }
+            );
+            lista_plikow.Sort(
+                delegate (FileInfo fi1, FileInfo fi2) { return fi1.CreationTime.CompareTo(fi2.CreationTime); }
+            );
+            wyswietlanie();
+        }
+
+        private void sortRozmiarButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            lista_plikow.Sort(
+                delegate (FileInfo fi1, FileInfo fi2) { return fi1.Length.CompareTo(fi2.Length); }
+            );
+            wyswietlanie();
+        }
+
+        private void wyswietlanie()
+        {
+            if (lista_katalogow.Count != 0)
+            {
+                listBox.Items.Add($"Liczba katalogow: {lista_katalogow.Count}");
+            }
+            if (lista_plikow.Count != 0)
+            {
+                listBox.Items.Add($"Liczba plikow: {lista_plikow.Count}");
+            }
+            if (lista_katalogow.Count != 0)
+            {
+                foreach (var element in lista_katalogow)
+                {
+                    listBox.Items.Add($"Katalog: {element.Name} data utworzenia: {element.CreationTime}");
+                }
+            }
+            if (lista_plikow.Count != 0)
+            {
+                foreach (var element in lista_plikow)
+                {
+                    listBox.Items.Add($"Plik: {element.Name} data utworzenia: {element.CreationTime} rozmiar pliku: {element.Length}");
+                }
+            }
+        }
+
+        private void zliczanieButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            Dictionary<string, int> typy = new Dictionary<string, int>();
+            foreach (var element in lista_plikow)
+            {
+                bool jest = false;
+                foreach (var typ in typy)
+                {
+                    if (typ.Key == element.Extension)
+                    {
+                        string napis = typ.Key;
+                        int ilosc = typ.Value + 1;
+                        typy.Remove(napis);
+                        typy.Add(napis, ilosc);
+                        jest = true;
+                        break;
+                    }
+                }
+                if (!jest)
+                {
+                    typy.Add(element.Extension, 1);
+                }
+            }
+            foreach (var element in typy)
+            {
+                listBox.Items.Add($"Sa {element.Value} plikow o typie {element.Key}");
+            }
+        }
+    }
+}
+
+
+/*
+        private void plikiButton_Click(object sender, EventArgs e)
+        {
+            refresh();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string katalog = fbd.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(katalog);
+                szukajPliki(directoryInfo);
+                informacja.Sort(delegate(FileInfo info1, FileInfo info2)
+                { return info1.CreationTime.CompareTo(info2.CreationTime); });
+                foreach (FileInfo element in informacja)
+                {
+                    listBox.Items.Add(element.Name + "   " + element.CreationTime + "   " + element.Length);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano katalogu do wyszukiwania");
+            }
+        }
+
+        private void katalogiButton_Click(object sender, EventArgs e)
+        {
+            refresh();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string katalog = fbd.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(katalog);
+                szukajKatalogi(directoryInfo);
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano katalogu do wyszukiwania");
+            }
+        }
+
+        private void szukajPliki(DirectoryInfo di)
+        {
+            
+            FileInfo[] pliki = di.GetFiles();
+            foreach (FileInfo fi in pliki)
+            {
+                informacja.Add(fi);
+            }
+            DirectoryInfo[] katalogi = di.GetDirectories();
+            foreach (DirectoryInfo dirinfo in katalogi)
+            {
+                szukajPliki(dirinfo);
+            }
+        }
+
+        private void szukajKatalogi(DirectoryInfo di)
+        {
+            DirectoryInfo[] katalogi = di.GetDirectories();
+            foreach (DirectoryInfo dirinfo in katalogi)
+            {
+                listBox.Items.Add(dirinfo.Name + " data utworzenia: " + dirinfo.CreationTime);
+                szukajKatalogi(dirinfo);
+            }
+        }
+
+        private void analizButton_Click(object sender, EventArgs e)
+        {
+            refresh();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string katalog = fbd.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(katalog);
+                listBox.Items.Add(directoryInfo.Name + " data utworzenia: " + directoryInfo.CreationTime);
+                analizujKatalog(directoryInfo);
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano katalogu do wyszukiwania");
+            }
+        }
+
+        private void analizujKatalog(DirectoryInfo di)
+        {
+            FileInfo[] pliki = di.GetFiles();
+            listBox.Items.Add("Pliki w katalogu ---> " + di.FullName + ":");
+            foreach (FileInfo fi in pliki)
+            {
+                listBox.Items.Add(fi.ToString() + " czas utworzenia: " + fi.CreationTime + " rozmiar pliku: " + fi.Length);
+            }
+            DirectoryInfo[] katalogi = di.GetDirectories();
+            foreach (DirectoryInfo dirinfo in katalogi)
+            {
+                analizujKatalog(dirinfo);
+            }
+        }
+
+        private void szukajButton_Click(object sender, EventArgs e)
+        {
+            refresh();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string katalog = fbd.SelectedPath;
+                DirectoryInfo directoryInfo = new DirectoryInfo(katalog);
+                szukaj(directoryInfo, searchTextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano katalogu do wyszukiwania");
+            }
+        }
+
+        private void szukaj(DirectoryInfo di, string pattern)
+        {
+            FileInfo[] pliki = di.GetFiles();
+            foreach (FileInfo fi in pliki)
+            {
+                if (fi.Name.Contains(pattern))
+                {
+                    listBox.Items.Add("Plik: " + fi.FullName + " czas utworzenia: " + fi.CreationTime + " rozmiar pliku: " + fi.Length);
+                }
+            }
+            DirectoryInfo[] katalogi = di.GetDirectories();
+            foreach (DirectoryInfo dirinfo in katalogi)
+            {
+                if (dirinfo.Name.Contains(pattern))
+                {
+                    listBox.Items.Add("Katalog: " + dirinfo.FullName + " czas utworzenia: " + dirinfo.CreationTime);
+                }
+                szukaj(dirinfo, pattern);
+            }
+
+        }
+*/
